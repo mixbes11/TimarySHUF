64fc6b7de2bab0b3511a2ef8ae7b64c196ff7ec6
Борис Михайлов <mixbes11@gmail.com>
2025-06-11 23:59:38 +0500

    Fixed

--- Timary/forms/timetable_form.py
+++ Timary/forms/timetable_form.py
@@ -7,6 +7,6 @@ class TimetableForm(FlaskForm):
     lesson = StringField('Урок', validators=[DataRequired()], render_kw={"placeholder": "  Урок"})
     room = StringField('Кабинет', validators=[DataRequired()], render_kw={"placeholder": "  Кабинет"})
     date = DateField('Дата', format='%d.%m.%Y', validators=[DataRequired()])
-    hour = IntegerField('Часы', validators=[DataRequired(), NumberRange(min=0, max=23)], render_kw={"placeholder": "ЧЧ"})
-    minute = IntegerField('Минуты', validators=[DataRequired(), NumberRange(min=0, max=59)], render_kw={"placeholder": "ММ"})
+    hour = IntegerField('Часы', validators=[NumberRange(min=0, max=23)], render_kw={"placeholder": "ЧЧ"})
+    minute = IntegerField('Минуты', validators=[NumberRange(min=0, max=59)], render_kw={"placeholder": "ММ"})
     submit = SubmitField('Добавить/Изменить')
